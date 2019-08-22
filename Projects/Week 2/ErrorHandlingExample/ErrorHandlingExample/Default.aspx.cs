3879978fa3850bebf4bae2ca87c6219c970e265d
Adam Rzonca <a.rzonca@yahoo.com>
2019-08-22 11:38:14 -0400

    Added Error Handling Project

---
+++ Projects/Week 2/ErrorHandlingExample/ErrorHandlingExample/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ErrorHandlingExample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Sum_Click(object sender, EventArgs e)
+        {
+            int sum = int.Parse(Number1.Text) + int.Parse(Number2.Text);
+            Trace.Write($"Sum is {sum}.");
+            System.Diagnostics.Debug.Assert(sum != 21);
+
+        }
+    }
+}
